eafafe0367bfc41ecd055fe12fd5066dcad265d6
fettahyildiz <fettah.yildiz@delivers.ai>
2023-02-13 13:32:28 +0300

    added initializer list

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -1,87 +1,89 @@
 #pragma once
 #include <iostream>
 
-
 using namespace std;
 
 namespace containers {
 
-	template <typename T>
+template <typename T>
 
-	struct Iterator {
-		
-		using iterator_category = std::bidirectional_iterator_tag;
-		using difference_type = std::ptrdiff_t;
-		using value_type = T;
-		using pointer = T*;  // or also value_type*
-		using reference = T&;  // or also value_type&
-		Iterator(pointer ptr) : m_ptr(ptr) {}
+struct Iterator {
 
-		reference operator*() const { return *m_ptr; }
-		pointer operator->() { return m_ptr; }
-		Iterator& operator++() { m_ptr++; return *this; }
-		Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
+  using iterator_category = std::bidirectional_iterator_tag;
+  using difference_type = std::ptrdiff_t;
+  using value_type = T;
+  using pointer = T *;   // or also value_type*
+  using reference = T &; // or also value_type&
+  Iterator(pointer ptr) : m_ptr(ptr) {}
 
-		friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
-		friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };
+  reference operator*() const { return *m_ptr; }
+  pointer operator->() { return m_ptr; }
+  Iterator &operator++() {
+    m_ptr++;
+    return *this;
+  }
+  Iterator operator++(int) {
+    Iterator tmp = *this;
+    ++(*this);
+    return tmp;
+  }
 
-	private:
-		pointer m_ptr;
-	};
+  friend bool operator==(const Iterator &a, const Iterator &b) {
+    return a.m_ptr == b.m_ptr;
+  };
+  friend bool operator!=(const Iterator &a, const Iterator &b) {
+    return a.m_ptr != b.m_ptr;
+  };
 
-	template <typename T>
+private:
+  pointer m_ptr;
+};
 
-	class vector {
-	private:
-		// Allocated data for the vector
-		unsigned int size;
-		// Current vector size
-		unsigned int current;
-		T* arr;
+template <typename T>
 
-	public:
-		vector() {
-			size = 1;
-			current = 0;
-			arr = new T[size];
-		}
-		vector(const int& size_) {
-			size = size_;
-			current = 0;
-			arr = new T[size];
-		}
-		vector(const int& size_, const T& val_) {
-			size = size_;
-			arr = new T[size];
-			
-			for (int i = 0; i < size_; i++) {
-				arr[i] = val_;
-			}
-			current = size_;
-		}
-		const unsigned int len() {
-			return current;
-		}
-		containers::Iterator<T> begin() { return Iterator(&arr[0]); }
-		containers::Iterator<T> end() { return Iterator(&arr[current]); }
-		void push_back(const T& data) {
-			if (current == size) {
-				cout << "DEBUG current" << current << '\n';
-				T* temp = new T[size * 2];
+class vector {
+private:
+  // Allocated data for the vector
+  unsigned int size;
+  // Current vector size
+  unsigned int current;
+  T *arr;
 
-				for (int i = 0; i < current; i++) {
-					temp[i] = arr[i];
-				}
-				// delete previous array
-				delete[] arr;
-				temp[current++] = data;
-				cout << "DEBUG current" << current << '\n';
-				arr = temp;
-			}
-			else {
-				arr[current++] = data;
-			}
-		}
+public:
+  vector() {
+    size = 1;
+    current = 0;
+    arr = new T[size];
+  }
+  vector(const int &size_) : size(size_) {
+    current = 0;
+    arr = new T[size];
+  }
+  vector(const int &size_, const T &val_) : size(size_), current(size_) {
+    arr = new T[size];
 
-	};
-}
+    for (int i = 0; i < size_; i++) {
+      arr[i] = val_;
+    }
+  }
+  ~vector() { delete[] arr; }
+  const unsigned int len() { return current; }
+  containers::Iterator<T> begin() { return Iterator(&arr[0]); }
+  containers::Iterator<T> end() { return Iterator(&arr[current]); }
+  void push_back(const T &data) {
+    if (current == size) {
+      T *temp = new T[size * 2];
+
+      for (int i = 0; i < current; i++) {
+        temp[i] = arr[i];
+      }
+      // delete previous array
+      delete[] arr;
+      temp[current++] = data;
+      arr = temp;
+    } else {
+      arr[current++] = data;
+    }
+  }
+};
+} // namespace containers
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include "Vector.hpp"
 #include <vector>
 int main() {
-	containers::vector<int> asd(10, 10);
-	for (auto i : asd) {
-		std::cout << i << " ";
-	}
-	std::cout << "size: " << asd.len() << '\n';
-	std::cout << '\n';
-	asd.push_back(44);
-	//containers::vector<int> *asd = new containers::vector<int>(10);
-	for (auto i : asd) {
-		std::cout << i << " ";
-	}
-	std::cout << "size: " << asd.len() << '\n';
+  containers::vector<double> asd(10, 10.0);
+  for (auto i : asd) {
+    std::cout << i << " ";
+  }
+  asd.push_back(5.5);
+  for (auto i : asd) {
+    std::cout << i << " ";
+  }
+  std::cout << "size: " << asd.len() << '\n';
 
-	return 0;
+  return 0;
 }
